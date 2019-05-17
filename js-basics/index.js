7a154b293d39e6d06a1fa4088890bdc419526612
akim157 <akim157@mail.ru>
2019-05-17 16:19:03 +0300

    v1.0.14

---
+++ js-basics/index.js
@@ -0,0 +1,15 @@
+let name = 'Mosh';
+console.log(name);
+
+//cannot be a reserved keyword
+//Should be meaningful
+//Cannot start with a number (1name)
+//Cannot contain a space or hyphen (-)
+// Are case-sensitive
+
+let firstName;
+let FirstName;
+
+let firstName = 'Maxim', lastName = 'Fedorov';
+console.log(firstName);
+console.log(lastName);
---
+++ js-basics/theory.js
@@ -0,0 +1,12 @@
+//в консоле с помощью node можно вызвать отработка скрипта
+node index.js
+//Variables - переменные
+//We use a variable to store data temporarily (Мы используем переменную для временного хранения данных)
+//Cannot be a reserved keyword (Нельзя использовать резервированные слова для объявления переменной такие как if, for...)
+//Should be meaningful (имена переменных должны быть значимыми т.е firstName)
+//Cannot start with a number (Нельзя начинать с чисел например: 1name)
+//Cannot contain a space or hyphen (Нельзя использовать пробелы или дифисы -)
+// Are case-sensitive (переменные чувствительны к регистру)
+let firstName = 'Maxim', lastName = 'Fedorov';
+console.log(firstName);
+console.log(lastName);
