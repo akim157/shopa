8596533f81cc1ed06f6e3f09e57f9d00e5b7ae10
akim157 <akim157@mail.ru>
2019-06-06 18:43:35 +0300

    v1.0.28

--- js-basics/theory.js
+++ js-basics/theory.js
@@ -1046,7 +1046,7 @@ const numbers = [1,2,3,-3];
 const atLeastOnePositive = numbers.some(function(value) {
     return value >= 0;
 });
-console.log(atLeastOnePositive); //false
+console.log(atLeastOnePositive); //true
 //Filtering an Array (Фильтрация массива)
 const numbers = [1, -1, 2, 3];
 const filtered = numbers.filter(function(value){
