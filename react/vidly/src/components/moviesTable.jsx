3ed7b5153dc10329204241d340855d829eb9177b
akim157 <akim157@mail.ru>
2019-06-16 02:08:15 +0300

    v1.5.35

--- react/vidly/src/components/moviesTable.jsx
+++ react/vidly/src/components/moviesTable.jsx
@@ -1,42 +1,49 @@
-import React, { Component } from 'react';
-import Table from './common/table';
-import Like from './common/like';
+import React, { Component } from "react";
+import { Link } from 'react-router-dom';
+import Table from "./common/table";
+import Like from "./common/like";
 
 // const x = <like></like>; //React Element {}
 
 class MoviesTable extends Component {
-    columns = [
-        { path: 'title', label: 'Title' },
-        { path: 'genre.name', label: 'Genre' },
-        { path: 'numberInStock', label: 'Stock' },
-        { path: 'dailyRentalRate', label: 'Rate' },
-        {
-            key: 'like',
-            content: movie => <Like liked={movie.liked} onClick={() => this.props.onLike(movie)}/>
-        },
-        {
-            key: 'delete',
-            content: movie => (
-                <button
-                    onClick={() => this.props.onDelete(movie)}
-                    className="btn btn-danger btn-sm"
-                >
-                    Delete
-                </button>
-            )
-        }
-    ];
-    render() {
-        const { movies, onSort, sortColumn } = this.props;
-        return (
-            <Table
-                columns={this.columns}
-                data={movies}
-                onSort={onSort}
-                sortColumn={sortColumn}
-            />
-        );
+  columns = [
+    {
+      path: "title",
+      label: "Title",
+      content: movie => <Link to={`/movies/${movie._id}`}>{movie.title}</Link>
+    },
+    { path: "genre.name", label: "Genre" },
+    { path: "numberInStock", label: "Stock" },
+    { path: "dailyRentalRate", label: "Rate" },
+    {
+      key: "like",
+      content: movie => (
+        <Like liked={movie.liked} onClick={() => this.props.onLike(movie)} />
+      )
+    },
+    {
+      key: "delete",
+      content: movie => (
+        <button
+          onClick={() => this.props.onDelete(movie)}
+          className="btn btn-danger btn-sm"
+        >
+          Delete
+        </button>
+      )
     }
+  ];
+  render() {
+    const { movies, onSort, sortColumn } = this.props;
+    return (
+      <Table
+        columns={this.columns}
+        data={movies}
+        onSort={onSort}
+        sortColumn={sortColumn}
+      />
+    );
+  }
 }
 
-export default MoviesTable
+export default MoviesTable;
