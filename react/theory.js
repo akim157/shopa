d945dddf4d3102c667333ca1bfeda61b1fd7f1f0
akim157 <akim157@mail.ru>
2019-06-19 18:51:18 +0300

    v1.7.39

--- react/theory.js
+++ react/theory.js
@@ -6848,6 +6848,182 @@ export default SearchBox;
 //Calling Backend Services (Вызов сервисов Backend)
 //create-react-app http-app (Создаем новый проект)
 /*=============== 137.JSON Placeholder (Заполнитель) ==================*/
+//https://jsonplaceholder.typicode.com/
+/*=============== 138.Http Clients (Http Клиенты) ==================*/
+//Front-end  <-|HTTP|-> Back-end
+//A lightweight library for building Uls (Легкая библиотека для сборки Uls)
+//State <-|-> View/DOM
+//----------------
+//React | Angular
+//Use your preferred libs (Используйте ваши любимые библиотеки) | Use HttpModule (использует HttpModule)
+//----------------
+//Fetch API (Получить API)
+//jQuery AJAX
+//Axios
+//----------------
+//npm i axios (устновка библиотеки)
+//https://github.com/axios/axios
+/*=============== 139.Getting Data (Получение данных) ==================*/
+//App.js
+import React, { Component } from "react";
+import axios from 'axios';
+import "./App.css";
+
+class App extends Component {
+    state = {
+        posts: []
+    };
+
+    async componentDidMount() {
+        //pending > resolved (success) OR rejected
+        const { data: posts } = await axios.get('https://jsonplaceholder.typicode.com/posts');
+        this.setState({ posts });
+        // const promise = axios.get('https://jsonplaceholder.typicode.com/posts');
+        // promise.then();
+        //   const response = await promise;
+        //   console.log(response);
+    }
+
+    handleAdd = () => {
+        console.log("Add");
+    };
+
+    handleUpdate = post => {
+        console.log("Update", post);
+    };
+
+    handleDelete = post => {
+        console.log("Delete", post);
+    };
+
+    render() {
+        return (
+            <React.Fragment>
+                <button className="btn btn-primary" onClick={this.handleAdd}>
+                    Add
+                </button>
+                <table className="table">
+                    <thead>
+                    <tr>
+                        <th>Title</th>
+                        <th>Update</th>
+                        <th>Delete</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    {this.state.posts.map(post => (
+                        <tr key={post.id}>
+                            <td>{post.title}</td>
+                            <td>
+                                <button
+                                    className="btn btn-info btn-sm"
+                                    onClick={() => this.handleUpdate(post)}
+                                >
+                                    Update
+                                </button>
+                            </td>
+                            <td>
+                                <button
+                                    className="btn btn-danger btn-sm"
+                                    onClick={() => this.handleDelete(post)}
+                                >
+                                    Delete
+                                </button>
+                            </td>
+                        </tr>
+                    ))}
+                    </tbody>
+                </table>
+            </React.Fragment>
+        );
+    }
+}
+
+export default App;
+/*=============== 140.Creating Data (Создание данных) ==================*/
+//app.js
+import React, { Component } from "react";
+import axios from 'axios';
+import "./App.css";
+
+const apiEndpoint = "https://jsonplaceholder.typicode.com/posts";
+
+class App extends Component {
+    state = {
+        posts: []
+    };
+
+    async componentDidMount() {
+        //pending > resolved (success) OR rejected
+        const { data: posts } = await axios.get(apiEndpoint);
+        this.setState({ posts });
+        // const promise = axios.get('https://jsonplaceholder.typicode.com/posts');
+        // promise.then();
+        //   const response = await promise;
+        //   console.log(response);
+    }
+
+    handleAdd = async () => {
+        const obj = { title: 'a', body: 'b' };
+        const { data: post } = await axios.post(apiEndpoint, obj);
+
+        const posts = [post, ...this.state.posts];
+        this.setState({ posts });
+    };
+
+    handleUpdate = post => {
+        console.log("Update", post);
+    };
+
+    handleDelete = post => {
+        console.log("Delete", post);
+    };
+
+    render() {
+        return (
+            <React.Fragment>
+                <button className="btn btn-primary" onClick={this.handleAdd}>
+                    Add
+                </button>
+                <table className="table">
+                    <thead>
+                    <tr>
+                        <th>Title</th>
+                        <th>Update</th>
+                        <th>Delete</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    {this.state.posts.map(post => (
+                        <tr key={post.id}>
+                            <td>{post.title}</td>
+                            <td>
+                                <button
+                                    className="btn btn-info btn-sm"
+                                    onClick={() => this.handleUpdate(post)}
+                                >
+                                    Update
+                                </button>
+                            </td>
+                            <td>
+                                <button
+                                    className="btn btn-danger btn-sm"
+                                    onClick={() => this.handleDelete(post)}
+                                >
+                                    Delete
+                                </button>
+                            </td>
+                        </tr>
+                    ))}
+                    </tbody>
+                </table>
+            </React.Fragment>
+        );
+    }
+}
+
+export default App;
+/*=============== 141.Lifecycle of a Request (Жизненный цикл запроса) ==================*/
 
 
 
