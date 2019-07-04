1e0f8a6a7a5df4d66c4cb30e6ecae6c2d4ece01b
akim157 <akim157@mail.ru>
2019-07-05 00:53:43 +0300

    v1.8.59

--- Node/vidly/index.js
+++ Node/vidly/index.js
@@ -1,6 +1,9 @@
+const Joi = require('joi');
+Joi.objectId = require('joi-objectid')(Joi);
 const mongoose = require('mongoose');
 const genres = require('./routes/genres');
 const customers = require('./routes/customers');
+const users = require('./routes/users');
 const express = require('express');
 const app = express();
 
@@ -11,6 +14,7 @@ mongoose.connect('mongodb://localhost/vidly')
 app.use(express.json());
 app.use('/api/genres', genres);
 app.use('/api/customers', customers);
+app.use('/api/users', users);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
